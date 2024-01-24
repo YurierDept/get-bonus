25dbd2c152ede2a79ceedf0720937c085d7c0984
XLor <yjl9903@vip.qq.com>
2024-01-24 17:37:43 +0800

    chore: add unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,23 @@ import { fileURLToPath } from 'node:url';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@unocss/nuxt'],
   devtools: { enabled: true },
+  css: ['@unocss/reset/tailwind.css'],
+  app: {
+    head: {
+      title: 'Get Bonus',
+      viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
+      htmlAttrs: {
+        lang: 'zh-CN'
+      },
+      link: [{ rel: 'icon', href: '/favicon.svg' }],
+      meta: []
+    }
+  },
+  unocss: {
+    preflight: true
+  },
   alias: {
     'get-bonus': fileURLToPath(new URL('./packages/get-bonus/src/index.ts', import.meta.url))
   }
