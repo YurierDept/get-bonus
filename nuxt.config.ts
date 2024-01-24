e6be42be040073b9f91fb6e03094b54d5cd5228b
XLor <yjl9903@vip.qq.com>
2024-01-24 17:31:59 +0800

    chore: init repo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
+import { fileURLToPath } from 'node:url';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  alias: {
+    'get-bonus': fileURLToPath(new URL('./packages/get-bonus/src/index.ts', import.meta.url))
+  }
+});
