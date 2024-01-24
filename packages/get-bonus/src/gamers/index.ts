05f6104fd04ad7fe7c82d2dfa416a89a0c662e61
KazariEX <1364035137@qq.com>
2024-01-25 01:45:06 +0800

    feat: add gamers provider

--- packages/get-bonus/src/gamers/index.ts
+++ packages/get-bonus/src/gamers/index.ts
@@ -1,17 +1,59 @@
 import type { Detail, SearchResult, SearchOptions } from '../types';
 
+import { JSDOM } from 'jsdom';
+import { ofetch } from 'ofetch';
+
 import { Provider } from '../scraper';
 
 export class Gamers extends Provider {
   constructor() {
-    super('gamers', '');
+    super('gamers', 'https://www.gamers.co.jp');
   }
 
   async search(text: string, options: SearchOptions): Promise<SearchResult[]> {
-    return [];
+    const html = await ofetch(this.baseUrl + '/products/list.php', {
+      query: {
+        smt: text,
+        spc: 4,
+        'nd[]': [5, 6]
+      }
+    });
+
+    const dom = new JSDOM(html);
+    const doc = dom.window.document;
+
+    const resultItems = doc.querySelectorAll('.list_product > a');
+    return [...resultItems].map((item) => {
+      const title = item.querySelector('.item_list_ttl');
+      return {
+        provider: this.id,
+        title: title?.textContent?.trim?.() || '',
+        url: this.baseUrl + (item as HTMLAnchorElement).href
+      };
+    });
   }
 
   async detail(url: string): Promise<Detail | undefined> {
-    return undefined;
+    const html = await ofetch(url);
+    const dom = new JSDOM(html);
+    const doc = dom.window.document;
+
+    const title = doc.querySelector('.ttl_style01');
+    const tokutens = doc.querySelectorAll('#tokuten > div[class]');
+    const items = [...tokutens].map((item) => {
+      const img = item.querySelector('img') as HTMLImageElement;
+      const info = item.querySelector('.tokuten_name');
+      return {
+        image: img.src,
+        description: info?.textContent || ''
+      };
+    });
+
+    return {
+      provider: this.id,
+      title: title?.textContent?.trim?.() || '',
+      url,
+      items
+    };
   }
 }
