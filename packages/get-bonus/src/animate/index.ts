ed3386f53400083baf8a2322196e66ebe430ce3b
XLor <yjl9903@vip.qq.com>
2024-01-24 23:33:12 +0800

    refactor: move to melon books

--- packages/get-bonus/src/animate/index.ts
+++ packages/get-bonus/src/animate/index.ts
@@ -11,50 +11,10 @@ export class Animate extends Provider {
   }
 
   async search(text: string, options: SearchOptions): Promise<SearchResult[]> {
-    const html: string = await ofetch('https://www.melonbooks.co.jp/search/search.php', {
-      query: {
-        name: text,
-        'additional[]': 'pr'
-      }
-    });
-
-    const dom = new JSDOM(html);
-    const doc = dom.window.document;
-
-    const resultItems = doc.querySelectorAll('.item-list li[class^=product]');
-    return [...resultItems].reduce((res: SearchResult[], item) => {
-      const a = item.querySelector('.item-image > a') as HTMLAnchorElement;
-      res.push({
-        provider: this.id,
-        title: a.title,
-        url: 'https://www.melonbooks.co.jp' + a.href
-      });
-      return res;
-    }, []);
+    return [];
   }
 
   async detail(url: string): Promise<Detail | undefined> {
-    const html: string = await $fetch(url);
-    const dom = new JSDOM(html);
-    const doc = dom.window.document;
-
-    const title = doc.querySelector('.page-header')?.textContent || '';
-    const privItems = doc.querySelectorAll('.priv-item');
-    const items = [...privItems].reduce((res: DetailItem[], item) => {
-      const img = item.querySelector('.priv_img') as HTMLImageElement;
-      const info = item.querySelector('.priv-item_info');
-      res.push({
-        image: img.src,
-        description: info?.textContent?.trim?.() || ''
-      });
-      return res;
-    }, []);
-
-    return {
-      provider: this.id,
-      title,
-      url,
-      items
-    };
+    return undefined;
   }
 }
--- packages/get-bonus/src/melonbooks/index.ts
+++ packages/get-bonus/src/melonbooks/index.ts
@@ -1,4 +1,7 @@
-import type { Detail, SearchResult, SearchOptions } from '../types';
+import type { Detail, SearchResult, SearchOptions, DetailItem } from '../types';
+
+import { JSDOM } from 'jsdom';
+import { ofetch } from 'ofetch';
 
 import { Provider } from '../scraper';
 
@@ -8,10 +11,58 @@ export class Melonbooks extends Provider {
   }
 
   async search(text: string, options: SearchOptions): Promise<SearchResult[]> {
-    return [];
+    const html: string = await ofetch('https://www.melonbooks.co.jp/search/search.php', {
+      query: {
+        name: text,
+        'additional[]': 'pr'
+      }
+    });
+
+    const dom = new JSDOM(html);
+    const doc = dom.window.document;
+
+    const resultItems = doc.querySelectorAll('.item-list li[class^=product]');
+    return [...resultItems].reduce((res: SearchResult[], item) => {
+      const a = item.querySelector('.item-image > a') as HTMLAnchorElement;
+      res.push({
+        provider: this.id,
+        title: a.title,
+        url: 'https://www.melonbooks.co.jp' + a.href
+      });
+      return res;
+    }, []);
   }
 
   async detail(url: string): Promise<Detail | undefined> {
-    return undefined;
+    const html: string = await ofetch(url);
+    const dom = new JSDOM(html);
+    const doc = dom.window.document;
+
+    const title = doc.querySelector('.page-header')?.textContent || '';
+    const privItems = doc.querySelectorAll('.priv-item');
+    const items = [...privItems].reduce((res: DetailItem[], item) => {
+      const img = item.querySelector('.priv_img') as HTMLImageElement;
+      const info = item.querySelector('.priv-item_info');
+      res.push({
+        image: enforceHTTPS(img.src),
+        description: info?.textContent?.trim?.() || ''
+      });
+      return res;
+    }, []);
+
+    return {
+      provider: this.id,
+      title,
+      url,
+      items
+    };
+  }
+}
+
+function enforceHTTPS(url: string) {
+  if (url.startsWith('//')) {
+    return 'https:' + url;
+  } else {
+    return url;
   }
 }
