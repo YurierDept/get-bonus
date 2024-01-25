e00672bbd0c5437d28180e8a5a74b5b919a27657
AkiPolaris <yuesluna@163.com>
2024-01-25 17:43:34 +0800

    feat: add comiczin provider

--- packages/get-bonus/src/comiczin/index.ts
+++ packages/get-bonus/src/comiczin/index.ts
@@ -1,17 +1,57 @@
 import type { Detail, SearchResult, SearchOptions } from '../types';
 
 import { Provider } from '../scraper';
+import { ofetch } from 'ofetch';
+import { JSDOM } from 'jsdom';
 
 export class Comiczin extends Provider {
   constructor() {
-    super('comiczin', '');
+    super('comiczin', 'https://shop.comiczin.jp');
   }
 
   async search(text: string, options: SearchOptions): Promise<SearchResult[]> {
-    return [];
+    const html = await ofetch(this.baseUrl + '/products/list.php',{
+      query:{
+        mode: 'search',
+        name: text
+      }
+    })
+
+    const dom = new JSDOM(html);
+    const doc = dom.window.document;
+
+    const resultItems = doc.querySelectorAll('div.list_item');
+    
+    return [...resultItems].map((item) => {
+      const a = item.querySelector('a.title_area') as HTMLAnchorElement;
+      return {
+        provider: this.id,
+        title: a.textContent?.trim?.() || '',
+        url: this.baseUrl + a.href
+      };
+    });
   }
 
   async detail(url: string): Promise<Detail | undefined> {
-    return undefined;
+    const html = await ofetch(url);
+    const dom = new JSDOM(html);
+    const doc = dom.window.document;
+
+    const title = doc.querySelector('.fw_main_block_header_type2');
+    const imgs = doc.querySelectorAll('img[id^=sample_img]');
+    const descs = doc.
+      querySelector('.div_block_main_item_comment')
+      ?.textContent;
+
+    const items = [...imgs].map((img,i) => ({
+      image: (img as HTMLImageElement).src,
+      description: descs?.substring(descs.lastIndexOf('【ZIN特典】')) || ''
+    }));
+    return {
+      provider: this.id,
+      title: title?.textContent?.trim() || '',
+      url,
+      items
+    };
   }
 }
