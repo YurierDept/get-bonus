2c00168d29e2ed0689debff2a45b92455267476b
XLor <yjl9903@vip.qq.com>
2024-01-25 15:41:39 +0800

    feat: add search cache

--- server/api/search/[input].ts
+++ server/api/search/[input].ts
@@ -1,19 +1,22 @@
 import { scraper } from '../../scraper';
 
-export default defineEventHandler(async (event) => {
-  const input = getRouterParam(event, 'input', {
-    decode: true
-  });
-  if (!input) {
-    throw new Error(`Input is not given`);
-  }
+export default defineCachedEventHandler(
+  async (event) => {
+    const input = getRouterParam(event, 'input', {
+      decode: true
+    });
+    if (!input) {
+      throw new Error(`Input is not given`);
+    }
 
-  const providersInfo = scraper.providers.map((p) => ({ id: p.id, baseUrl: p.baseUrl }));
-  const searchResult = await scraper.search(input);
-  const details = await scraper.getAllDetails(searchResult);
+    const providersInfo = scraper.providers.map((p) => ({ id: p.id, baseUrl: p.baseUrl }));
+    const searchResult = await scraper.search(input);
+    const details = await scraper.getAllDetails(searchResult);
 
-  return {
-    providers: providersInfo,
-    details
-  };
-});
+    return {
+      providers: providersInfo,
+      details
+    };
+  },
+  { maxAge: 60 }
+);
