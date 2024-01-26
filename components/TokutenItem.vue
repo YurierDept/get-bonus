9f88254a22881ca9f9dbbc5abde07079dc936837
XLor <yjl9903@vip.qq.com>
2024-01-26 16:01:00 +0800

    feat: add icon

--- components/TokutenItem.vue
+++ components/TokutenItem.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type { DetailItem } from 'get-bonus';
 
+import { Download, Copy, ClipboardCopy } from 'lucide-vue-next';
+
 const props = defineProps<{
   data: DetailItem;
 }>();
@@ -23,10 +25,16 @@ function copyDescription() {
     <img class="max-w-36" :src="data.image" />
     <div class="grid grid-rows-[1fr_auto] gap-2">
       <p class="text-3.5 whitespace-pre-line">{{ data.description }}</p>
-      <div class="flex gap-2">
-        <Button @click="downloadImage">下载图片</Button>
-        <Button @click="copyImage">复制图片</Button>
-        <Button @click="copyDescription">复制描述</Button>
+      <div class="flex gap-4">
+        <Button @click="downloadImage" variant="secondary" size="sm"
+          ><Download class="w-4 h-4 mr-2" />下载图片</Button
+        >
+        <Button @click="copyImage" variant="secondary" size="sm"
+          ><Copy class="w-4 h-4 mr-2"></Copy>复制图片</Button
+        >
+        <Button @click="copyDescription" variant="secondary" size="sm"
+          ><ClipboardCopy class="w-4 h-4 mr-2"></ClipboardCopy>复制描述</Button
+        >
       </div>
     </div>
   </li>
