2682f5c49d086cec3741f8a20f508c13211a1626
XLor <yjl9903@vip.qq.com>
2024-01-26 16:49:23 +0800

    feat: copy title

--- components/ResultItem.vue
+++ components/ResultItem.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import type { Detail } from 'get-bonus';
 
+import { toast } from 'vue-sonner';
+import { Copy } from 'lucide-vue-next';
+
 const props = defineProps<{
   data: Detail;
 }>();
@@ -8,13 +11,23 @@ const props = defineProps<{
 const baseUrl = computed(() => {
   return new URL(props.data.url).origin;
 });
+
+async function copyTitle() {
+  await navigator.clipboard.writeText(props.data.title);
+  toast.success(`复制标题成功`, {});
+}
 </script>
 
 <template>
   <li class="p-4 b-1 rounded-4">
-    <nuxt-link class="font-bold text-5 hover:color-blue" :to="data.url" target="_blank">
-      <span>{{ data.title }}</span>
-    </nuxt-link>
+    <span>
+      <nuxt-link class="font-bold text-5 hover:color-blue" :to="data.url" target="_blank">
+        <span>{{ data.title }}</span>
+      </nuxt-link>
+      <Button variant="ghost" size="icon" class="ml-2" @click="copyTitle">
+        <Copy class="w-4 h-4" />
+      </Button>
+    </span>
     <span class="mt-2 flex items-center gap-2">
       <a :href="baseUrl" target="_blank">
         <Badge variant="outline" class="select-none">{{ data.provider }}</Badge>
