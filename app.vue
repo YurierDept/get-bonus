e5357d79cb79b4791098958422bbca0c4ac8bcea
XLor <yjl9903@vip.qq.com>
2024-02-04 00:59:21 +0800

    feat: add clarity

--- app.vue
+++ app.vue
@@ -4,6 +4,12 @@ import { Toaster } from '@/components/ui/sonner';
 </script>
 
 <template>
+  <Script type="text/javascript">
+    (function (c, l, a, r, i, t, y) { c[a] = c[a] || function () { (c[a].q = c[a].q ||
+    []).push(arguments); }; t = l.createElement(r); t.async = 1; t.src =
+    'https://www.clarity.ms/tag/' + i; y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y); })(window, document, 'clarity', 'script', 'kw32lgez2o');
+  </Script>
   <Toaster />
   <NuxtLoadingIndicator />
   <NuxtLayout>
