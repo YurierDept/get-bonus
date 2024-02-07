91d0d1559068ee7c771bff21afc9488454d5d9c3
arcHarico <harico@yurier.net>
2024-02-07 14:42:46 +0800

    feat: Toaster changes(richColors and position)

--- app.vue
+++ app.vue
@@ -10,7 +10,7 @@ import { Toaster } from '@/components/ui/sonner';
     'https://www.clarity.ms/tag/' + i; y = l.getElementsByTagName(r)[0];
     y.parentNode.insertBefore(t, y); })(window, document, 'clarity', 'script', 'kw32lgez2o');
   </Script>
-  <Toaster />
+  <Toaster richColors position="top-center"/>
   <NuxtLoadingIndicator />
   <NuxtLayout>
     <NuxtLoadingIndicator />
