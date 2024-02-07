0f9f7ebd0881fe0da3e02b5d1988ae2eb1de5d8a
XLor <yjl9903@vip.qq.com>
2024-02-07 16:32:05 +0800

    feat: move clarity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,13 @@ export default defineNuxtConfig({
         lang: 'zh-CN'
       },
       link: [{ rel: 'icon', href: '/favicon.ico' }],
-      meta: []
+      meta: [],
+      script: [
+        {
+          type: 'text/javascript',
+          children: `(function (c, l, a, r, i, t, y) { c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments); }; t = l.createElement(r); t.async = 1; t.src = 'https://www.clarity.ms/tag/' + i; y = l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t, y); })(window, document, 'clarity', 'script', 'kw32lgez2o');`
+        }
+      ]
     }
   },
   image: {
