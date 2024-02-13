f53b7bf55823c315e11374630def4f955e8e6f91
KazariEX <1364035137@qq.com>
2024-02-13 22:36:39 +0800

    chore: prettier format

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,38 +7,30 @@ import { Milestone, Clock3, Sparkles } from 'lucide-vue-next';
 </script>
 
 <template>
-  <footer class="space-y-2 mb-10">
-    <div class="text-sm text-base-600 text-center">
-      <span>
-        <button>
-          <Milestone class="w-3 h-3 ml-1"></Milestone>
-        </button>
-        程序版本号: {{ version }} ( {{ sha.slice(0, 6) }} ) 
-      </span>
-    </div>
-    <div class="text-sm text-base-600 text-center">
-      <span>
+  <footer class="space-y-2 mb-10 text-sm text-center text-base-600">
+    <p>
       <button>
-          <Clock3 class="w-3 h-3 ml-2"></Clock3>
+        <Milestone class="w-3 h-3 ml-1"></Milestone>
       </button>
-      程序构建于: {{ buildTime }} 
-      </span>
-    </div>
-    <div class="flex justify-center text-sm">
-      <span>
-        <button>
-          <Sparkles class="w-3 h-3 ml-2"></Sparkles>
-        </button>
+      程序版本号: {{ version }} ( {{ sha.slice(0, 6) }} )
+    </p>
+    <p>
+      <button>
+        <Clock3 class="w-3 h-3 ml-2"></Clock3>
+      </button>
+      程序构建于: {{ buildTime }}
+    </p>
+    <p>
+      <button>
+        <Sparkles class="w-3 h-3 ml-2"></Sparkles>
+      </button>
       「百合花船·特典搜索」项目是
       <a
         href="https://yurier.net/projects#%E6%9C%8D%E5%8A%A1-%E3%80%8C%E7%99%BE%E5%90%88%E8%8A%B1%E8%88%B9%E3%80%8D%E4%BC%81%E5%88%92"
         target="_blank"
         color="#45c2ff"
-      >
-        「百合花船」企划
-      </a>
-      的一部分。
-    </span>
-    </div>
+        >「百合花船」企划</a
+      >的一部分。
+    </p>
   </footer>
 </template>
