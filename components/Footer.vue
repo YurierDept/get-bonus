0884f40fd4f51782d33c3aa83499145c4bf030db
XLor <yjl9903@vip.qq.com>
2024-02-20 15:24:43 +0800

    refactor: footer html and version link

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,30 +7,33 @@ import { Milestone, Clock3, Sparkles } from 'lucide-vue-next';
 </script>
 
 <template>
-  <footer class="space-y-2 mb-10 text-sm text-center text-base-600">
+  <footer class="space-y-2 mb-10 text-sm text-center text-base-500">
     <p>
-      <button>
-        <Milestone class="w-3 h-3 ml-1"></Milestone>
-      </button>
-      程序版本号: {{ version }} ( {{ sha.slice(0, 6) }} )
+      <Milestone class="inline align-baseline w-3 h-3 mr-1"></Milestone>
+      <span
+        >程序版本号: {{ version }} (<a
+          :href="`https://github.com/YurierDept/get-bonus/tree/${sha}`"
+          target="_blank"
+          class="hover:text-[#45c2ff]"
+          >{{ sha.slice(0, 6) }}</a
+        >)</span
+      >
     </p>
     <p>
-      <button>
-        <Clock3 class="w-3 h-3 ml-2"></Clock3>
-      </button>
-      程序构建于: {{ buildTime }}
+      <Clock3 class="inline align-baseline w-3 h-3 mr-1"></Clock3>
+      <span>程序构建于: {{ buildTime }}</span>
     </p>
     <p>
-      <button>
-        <Sparkles class="w-3 h-3 ml-2"></Sparkles>
-      </button>
-      「百合花船·特典搜索」项目是
-      <a
-        href="https://yurier.net/projects#%E6%9C%8D%E5%8A%A1-%E3%80%8C%E7%99%BE%E5%90%88%E8%8A%B1%E8%88%B9%E3%80%8D%E4%BC%81%E5%88%92"
-        target="_blank"
-        color="#45c2ff"
-        >「百合花船」企划</a
-      >的一部分。
+      <Sparkles class="inline align-baseline w-3 h-3 mr-1"></Sparkles>
+      <span>
+        <span>「百合花船·特典搜索」项目是</span
+        ><a
+          href="https://yurier.net/projects#%E6%9C%8D%E5%8A%A1-%E3%80%8C%E7%99%BE%E5%90%88%E8%8A%B1%E8%88%B9%E3%80%8D%E4%BC%81%E5%88%92"
+          target="_blank"
+          color="#45c2ff"
+          >「百合花船」企划</a
+        ><span> 的一部分。</span>
+      </span>
     </p>
   </footer>
 </template>
