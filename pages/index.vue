c1e5f1efcdee8929cb36942ef8b9ebdebf2c1cf2
XLor <yjl9903@vip.qq.com>
2024-02-20 15:50:05 +0800

    chore: fix mobile layouts

--- pages/index.vue
+++ pages/index.vue
@@ -350,6 +350,6 @@ onMounted(() => {
 
 <style>
 .main {
-  @apply: mx-auto xl:w-5xl lg:w-3xl md:w-2xl lt-md:w-[95vw] lt-md:px-3 lt-sm:w-[100vw] lt-sm:px-2;
+  @apply: mx-auto xl:w-5xl lg:w-3xl md:w-2xl lt-md:w-[95vw] lt-md:px-3 lt-sm:w-[100vw];
 }
 </style>
